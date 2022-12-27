c8c312f29b866a4036a10aa94b94e0ae61123b3f
Gaurav Mahor <gaurav.mahor@nagarro.com>
2022-12-28 01:54:48 +0530

    Subtraction feature added

--- src/Maths/Addition.java
+++ src/Maths/Addition.java
@@ -1,3 +1,5 @@
+package Maths;
+
 public class Addition
 {
 	public static void main(String[] args)
---
+++ src/Maths/Subtraction.java
@@ -0,0 +1,10 @@
+package Maths;
+
+public class Subtraction
+{
+	public static void main(String[] args)
+	{
+		int num1=20, num2=10;
+		System.out.print("Subtraction of "+num1+" and "+num2+" is: "+(num1-num2));
+	}
+}

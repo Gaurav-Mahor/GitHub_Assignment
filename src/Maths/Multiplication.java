465f217f5c4eac18b928baba1728702db87f9a15
Gaurav Mahor <gaurav.mahor@nagarro.com>
2022-12-28 01:57:17 +0530

    Multiplication feature added

---
+++ src/Maths/Multiplication.java
@@ -0,0 +1,10 @@
+package Maths;
+
+public class Multiplication
+{
+	public static void main(String[] args)
+	{
+		int num1=20, num2=10;
+		System.out.print("Multiplication of "+num1+" and "+num2+" is: "+num1*num2);
+	}
+}
